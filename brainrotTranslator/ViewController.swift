4030e79aa28d2219d225a2d8f36676e4c4833e4d
EvanSwims <evanswimming@gmail.com>
2025-02-27 07:39:23 -0600

    Initial Commit

---
+++ brainrotTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  brainrotTranslator
+//
+//  Created by EVAN MEYER on 2/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ brainrotTranslatorTests/brainrotTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  brainrotTranslatorTests.swift
+//  brainrotTranslatorTests
+//
+//  Created by EVAN MEYER on 2/27/25.
+//
+
+import Testing
+@testable import brainrotTranslator
+
+struct brainrotTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
